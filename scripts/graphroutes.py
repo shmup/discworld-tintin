7671e8e47cb85e5a0c5127c1705fab989ef90c20
Jared Miller <jared@smell.flowers>
2024-02-17 17:38:17 -0500

    Clean up graphroutes.py

--- scripts/graphroutes.py
+++ scripts/graphroutes.py
@@ -2,54 +2,53 @@
 
 from graphviz import Graph
 
-font_name = 'Pragmata Pro'
-font_size = '13'
-node_shape = 'plaintext'
-node_margin = '0.1'
-edge_thickness = '1.5'
-image_format = 'png'
-output_filename = 'donkroutes'
-node_color = 'black'
-edge_color = 'blue'
-background_color = '#dddddd'
+config = {
+    'font_name': 'Pragmata Pro',
+    'font_size': '13',
+    'node_shape': 'plaintext',
+    'node_margin': '0.1',
+    'edge_thickness': '1.5',
+    'image_format': 'png',
+    'output_filename': 'donkroutes',
+    'node_color': '#000000',  # black
+    'edge_color': '#0000ff',  # blue
+    'background_color': '#dddddd'
+}
 
 G = Graph('G', filename='network.gv', engine='neato')
-G.attr(bgcolor=background_color)
-G.attr('node', fontcolor=node_color)
-G.attr('edge', color=edge_color)
+G.attr(bgcolor=config['background_color'])
+G.attr('node', fontcolor=config['node_color'])
+G.attr('edge', color=config['edge_color'])
 
-added_nodes = set()
-added_edges = set()
+added_nodes, added_edges = set(), set()
 
-with open('rls.txt', 'r') as file:
+with open('rls.txt') as file:
     for line in file:
         parts = line.strip().replace(' and ',
                                      ', ').replace('connected to ',
                                                    '').split(', ')
-        main_node = parts[0]
-        connected_nodes = parts[1:]
+        node, connected_nodes = parts[0], parts[1:]
 
-        if main_node not in added_nodes:
-            G.node(main_node,
-                   fontname=font_name,
-                   fontsize=font_size,
-                   shape=node_shape,
-                   margin=node_margin)
-            added_nodes.add(main_node)
+        if node not in added_nodes:
+            G.node(node,
+                   fontname=config['font_name'],
+                   fontsize=config['font_size'],
+                   shape=config['node_shape'],
+                   margin=config['node_margin'])
+            added_nodes.add(node)
 
         for connected_node in connected_nodes:
             if connected_node not in added_nodes:
                 G.node(connected_node,
-                       fontname=font_name,
-                       fontsize=font_size,
-                       shape=node_shape,
-                       margin=node_margin)
+                       fontname=config['font_name'],
+                       fontsize=config['font_size'],
+                       shape=config['node_shape'],
+                       margin=config['node_margin'])
                 added_nodes.add(connected_node)
 
-            edge = tuple(sorted((main_node, connected_node)))
-
+            edge = tuple(sorted((node, connected_node)))
             if edge not in added_edges:
                 G.edge(*edge)
                 added_edges.add(edge)
 
-G.render('donkroutes', format='png', view=False)
+G.render(config['output_filename'], format='png')
